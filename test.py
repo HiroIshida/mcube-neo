782676ac4875ac4dda7d42d6c0c6381029caf341
HiroIshida <spitfire.docomo@gmail.com>
2020-09-26 04:52:39 +0900

    dfs with queue is tested

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from mpl_toolkits.mplot3d import Axes3D
 import build.mcube as mcube
 
-N = 15
+N = 50
 w = 1.2
 x = y = z = np.linspace(-w, w, N) 
 X, Y, Z = np.meshgrid(x, y, z)
@@ -34,7 +34,6 @@ def dfs(E, N):
             face = E[idx_face]
             for idx_vert in face:
                 if not isVisited[idx_vert]:
-                    print("hoge")
                     recursion(idx_vert, isVisited, group)
 
     def make_group(idx_init):
@@ -44,7 +43,6 @@ def dfs(E, N):
 
     group_list = []
     while(True):
-        print("hoge")
         pair = next(((x, idx) for (x, idx) in \
                 zip(list(isVisited), range(len(isVisited))) if ~x), None)
         if pair is None:
@@ -53,9 +51,39 @@ def dfs(E, N):
 
     return isVisited, group_list
 
-ts = time()
-isVisited, group_list = dfs(E, NF)
-print(time() - ts)
+def dfs_queue(E, N):
+    from queue import LifoQueue
+    isVisited = np.array([False]*len(V))
+
+    def make_group(idx_init):
+        group = []
+        q = LifoQueue()
+        isVisited[idx_init] = True
+        q.put(idx_init)
+        while not q.empty():
+            idx_here = q.get()
+            face_near_idxes = NF[idx_here]
+            for face_near_idx in face_near_idxes:
+                face = E[face_near_idx]
+                for idx_cand in face:
+                    if not isVisited[idx_cand]:
+                        isVisited[idx_cand] = True
+                        q.put(idx_cand)
+                        group.append(idx_cand)
+        return group
+
+    group_list = []
+    while(True):
+        pair = next(((x, idx) for (x, idx) in \
+                zip(list(isVisited), range(len(isVisited))) if ~x), None)
+        if pair is None:
+            break
+        group_list.append(make_group(pair[1]))
+
+    return isVisited, group_list
+
+isVisited, group_list = dfs_queue(E, NF)
+
 import matplotlib.cm as cm
 cmap = cm.get_cmap(name='rainbow')
 
